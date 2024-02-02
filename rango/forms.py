4053a6b9244855017cd7564e6e70db755c741631
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-02-02 10:00:28 +0000

    chapter 7

---
+++ rango/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import *
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128,help_text="Please enter the category name.")
+    # deleted the likes, views, and slug because they're initialised in model
+    # can't actually delete because of tests
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    
+    class Meta:
+        model = Category
+        fields = ("name",)
+
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    
+    class Meta:
+        model = Page
+        exclude = ('category',)
+        
+    # overriden method
+    def clean(self):
+        cleaned_data = self.cleaned_data 
+        url = cleaned_data.get('url')
+        # If url is not empty and doesn't start with 'http://', # then prepend 'http://'.
+        # this is absolutely retarded and doesn't work (because normal pages are https), but the tests require it
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+          
+        print("AAAAAAA", url)
+        return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.urls import reverse
+
+from .forms import CategoryForm,PageForm
 from .models import Category, Page
 
 def index(request):
@@ -25,3 +28,51 @@ def show_category(request, category_name_slug):
         
     print(context_dict.values())
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+    
+    if request.method =="POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            # commit=True by default
+            form.save()
+
+            return redirect(reverse("rango:index"))
+            
+        else:
+            print(form.errors)
+            
+    return render(request, 'rango/add_category.html', {'form': form})
+    
+    
+    
+def add_page(request, category_name_slug):
+    form = PageForm()
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+        
+    if category is None:
+        return redirect(reverse("rango:index"))
+    
+    if request.method =="POST":
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+            
+        else:
+            print(form.errors)
+            return redirect(reverse("rango:index"))
+            
+    # Get
+    else:
+        return render(request, 'rango/add_page.html', {'form': form, 'category': category})
