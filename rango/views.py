58377f6085e7e433669626cd4c618ec606c25ad0
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-02-07 00:26:29 +0000

    9.6

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.urls import reverse
 
-from .forms import CategoryForm,PageForm
+from .forms import *
 from .models import Category, Page
 
 def index(request):
@@ -76,3 +76,40 @@ def add_page(request, category_name_slug):
     # Get
     else:
         return render(request, 'rango/add_page.html', {'form': form, 'category': category})
+    
+    
+    
+def register(request):
+    registered = False
+    
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            
+            profile = profile_form.save(commit=False)
+            profile.user = user # links the 2 models together
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+                
+            profile.save()
+            registered = True
+        
+        else:
+            print(user_form.errors, profile_form.errors)
+    
+    # GET        
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+        
+        
+    # GET or POST
+    return render(request, 'rango/register.html',
+                  context = {'user_form': user_form,
+                             'profile_form': profile_form,
+                             'registered': registered})
