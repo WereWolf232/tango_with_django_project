0bd03ff02fab83f246cff53c32e0db4ae39324c6
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-03-21 16:16:32 +0000

    final fix

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR / "templates" #pathlib paths automatically adjust the slash dependent on the OS
+
+#pathlib paths automatically adjust the slash dependent on the OS
+TEMPLATE_DIR = BASE_DIR / "templates" 
 STATIC_DIR = BASE_DIR / "static"
 MEDIA_DIR = BASE_DIR / "media"
 
