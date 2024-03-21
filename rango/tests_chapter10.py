6ef85afaec53eb0d45177c7fe866751cb24c549a
Steve Falkovsky <2782416F@student.gla.ac.uk>
2024-03-21 17:30:35 +0000

    asd

--- rango/tests_chapter10.py
+++ rango/tests_chapter10.py
@@ -40,6 +40,9 @@ class Chapter10ConfigurationTests(TestCase):
     """
     Tests the configuration of the Django project -- can cookies be used, at least on the server-side?
     """
+    def test_about_view(self):
+        self.assertFalse(True)
+        
     def test_middleware_present(self):
         """
         Tests to see if the SessionMiddleware is present in the project configuration.
